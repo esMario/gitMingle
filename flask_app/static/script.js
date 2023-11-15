a9e59b9cf86de13fe82985c9027a9c6c03243f58
Mario L <esmariolongoria@gmail.com>
2023-11-14 20:18:12 -0600

    Updated Show Project Page + Index Animations

--- flask_app/static/script.js
+++ flask_app/static/script.js
@@ -1,3 +1,4 @@
+/*  HOME PAGE LOGO SCROLLER */
 function copyAndAddLogos() {
     var logosSlide = document.querySelector(".logos-slide");
     // Check if an element with class "logos-slide" exists 
@@ -7,5 +8,21 @@ function copyAndAddLogos() {
         document.querySelector('.logos').appendChild(copy);
     }
 }
-
 copyAndAddLogos();
+
+/* GRAB CURRENT LINK BUTTON */
+$('#current_link_btn').on('click', function (e) {
+    var dummy = document.createElement('input');
+    var text = window.location.href;
+
+    // @see https://stackoverflow.com/a/49618964
+    document.body.appendChild(dummy);
+    dummy.value = text;
+    dummy.select();
+    document.execCommand('copy');
+    document.body.removeChild(dummy);
+    $(".copy_indicate").text("URL copied!");
+});
+    
+
+/* DASHBOARD/HUB CAROUSEL AUTO SCROLLER */
